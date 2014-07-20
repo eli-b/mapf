e47a6dc8afacc0fda48d2a36502f3e68ab454aa6
Eli Boyarski <Eli Boyarski@localhost>
2014-07-20 17:30:55 +0300

    DRLA*: More statistics (move to multidimensional arrays debatable), small fixes

--- DynamicRationalLazyOpenList.cs
+++ DynamicRationalLazyOpenList.cs
@@ -12,12 +12,13 @@ namespace CPF_experiment
         protected int lastF;
         protected int skips;
         protected int accSkips;
-        protected double[][] capData;
-        protected double[][] accCapData;
+        protected double[,] capData;
+        protected double[,] accCapData;
         protected static readonly double MOVING_AVERAGE_FACTOR = 1;
         protected static readonly int SUCCESS_IND = 0;
         protected static readonly int FAILURE_IND = 1;
         protected static readonly int PH_IND = 2;
+        protected static readonly int USES_IND = 3;
         protected static readonly int NUM_CAPS = 27;
         private double expandStartTime;
         protected double sumExpandTimes;
@@ -30,6 +31,7 @@ namespace CPF_experiment
         {
             this.expensive = expensive;
             this.ClearPrivateStatistics();
+            this.ClearPrivateAccumulatedStatistics();
             this.runner = runner;
         }
 
@@ -40,19 +42,20 @@ namespace CPF_experiment
             this.numExpands = 0;
             this.sumExpandTimes = 0;
             this.skips = 0;
-            this.capData = new double[DynamicRationalLazyOpenList.NUM_CAPS][];
-            this.accCapData = new double[DynamicRationalLazyOpenList.NUM_CAPS][];
-            for (int i = 0; i < DynamicRationalLazyOpenList.NUM_CAPS; ++i)
-            {
-                this.capData[i] = new double[3] { 0, 0, 0 };
-                this.accCapData[i] = new double[3] { 0, 0, 0 };
-            }
+            this.capData = new double[DynamicRationalLazyOpenList.NUM_CAPS, 4];
+        }
+
+        protected void ClearPrivateAccumulatedStatistics()
+        {
+            this.accNumExpands = 0;
+            this.accSumExpandTimes = 0;
+            this.accSkips = 0;
+            this.accCapData = new double[DynamicRationalLazyOpenList.NUM_CAPS, 4];
         }
 
         public override string ToString()
         {
-            return "DynamicRationalLazyOpenList(alpha=" + DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR + 
-                   " num_caps=" + DynamicRationalLazyOpenList.NUM_CAPS + ")/" + this.expensive;
+            return "DynamicRationalLazyOpenList/" + this.expensive;
         }
 
         public override IBinaryHeapItem Remove()
@@ -102,7 +105,7 @@ namespace CPF_experiment
 
                 //double tempStartTime = ((DyanamicLazyCbsh)this.expensive).runner.ElapsedMilliseconds();
 
-                // Compute the expected regret from choosing each cap, and finding cap that gives the lowest expected regret
+                // Computing the expected regret from choosing each cap, and finding the cap that gives the lowest expected regret
                 for (int i=0 ; i < DynamicRationalLazyOpenList.NUM_CAPS ; ++i)
                 {
                     expectedRegret[i] = 0;
@@ -112,20 +115,20 @@ namespace CPF_experiment
                         cap = 1 << j; // microseconds
                         double pThatThisCapWouldBeTheFirstCapToSucceed;
                         if (j != 0)
-                            pThatThisCapWouldBeTheFirstCapToSucceed = this.capData[j][DynamicRationalLazyOpenList.PH_IND] - this.capData[j - 1][DynamicRationalLazyOpenList.PH_IND]; // The PH_IND points to the probability that the given cap would succeed at all, including the probability that it would've worked for lower caps.
+                            pThatThisCapWouldBeTheFirstCapToSucceed = this.capData[j, DynamicRationalLazyOpenList.PH_IND] - this.capData[j - 1, DynamicRationalLazyOpenList.PH_IND]; // The PH_IND points to the probability that the given cap would succeed at all, including the probability that it would've worked for lower caps.
                         else
-                            pThatThisCapWouldBeTheFirstCapToSucceed = this.capData[j][DynamicRationalLazyOpenList.PH_IND];
-                        expectedRegret[i] += (0.75 * cap/*cap*/ + t0 - Math.Min(0.75 * cap/*cap*/ + t0, tExpand)) * pThatThisCapWouldBeTheFirstCapToSucceed;  // The cap is an upper bound on t2. A closer estimate would be 0.75*cap (since 0.5*cap is the previous cap).
+                            pThatThisCapWouldBeTheFirstCapToSucceed = this.capData[j, DynamicRationalLazyOpenList.PH_IND];
+                        expectedRegret[i] += (0.75 * cap + t0 - Math.Min(0.75 * cap + t0, tExpand)) * pThatThisCapWouldBeTheFirstCapToSucceed;  // The cap is an upper bound on t2. A closer estimate would be 0.75*cap (since 0.5*cap is the previous cap).
                     }
                     for (int j=i+1 ; j < DynamicRationalLazyOpenList.NUM_CAPS; ++j)
                     {
                         double heuristicOnlyHelpfulAfter = 1 << j; // microseconds
-                        double pThatThisCapWouldBeTheFirstCapToSucceed = this.capData[j][DynamicRationalLazyOpenList.PH_IND] - this.capData[j - 1][DynamicRationalLazyOpenList.PH_IND];
-                        expectedRegret[i] += (cap + tExpand - Math.Min(0.75 * heuristicOnlyHelpfulAfter/*heuristicOnlyHelpfulAfter*/ + t0, tExpand)) * pThatThisCapWouldBeTheFirstCapToSucceed;
+                        double pThatThisCapWouldBeTheFirstCapToSucceed = this.capData[j, DynamicRationalLazyOpenList.PH_IND] - this.capData[j - 1, DynamicRationalLazyOpenList.PH_IND];
+                        expectedRegret[i] += (cap + tExpand - Math.Min(0.75 * heuristicOnlyHelpfulAfter + t0, tExpand)) * pThatThisCapWouldBeTheFirstCapToSucceed;
                     }
                     // The case where even the highest cap fails:
                     int last = DynamicRationalLazyOpenList.NUM_CAPS - 1;
-                    expectedRegret[i] += (1 << last) * (1 - this.capData[last][DynamicRationalLazyOpenList.PH_IND]);
+                    expectedRegret[i] += (1 << last) * (1 - this.capData[last, DynamicRationalLazyOpenList.PH_IND]);
 
                     if (expectedRegret[i] < lowestRegret)
                     {
@@ -139,8 +142,8 @@ namespace CPF_experiment
                 // and fail. This has the added benefit that after running, we'll have an informed belief for all lower caps and possibly
                 // all higher caps too.
                 int nextCapAboveTExpand = (int)Math.Ceiling(Math.Log(tExpand, 2));
-                if (this.capData[nextCapAboveTExpand][DynamicRationalLazyOpenList.SUCCESS_IND] +
-                    this.capData[nextCapAboveTExpand][DynamicRationalLazyOpenList.FAILURE_IND] == 0) // This cap was never tried before, and it might be good.
+                if (this.capData[nextCapAboveTExpand, DynamicRationalLazyOpenList.SUCCESS_IND] +
+                    this.capData[nextCapAboveTExpand, DynamicRationalLazyOpenList.FAILURE_IND] == 0) // This cap was never tried before, and it might be good.
                 {
                     selectedCapExponent = nextCapAboveTExpand;
                 } // TODO: Put the entire matrix calculation above into the else of this clause.
@@ -153,13 +156,15 @@ namespace CPF_experiment
                 //Console.WriteLine("Texpand:{0}={1}/{2}", tExpand, this.sumExpandTimes * 1000, this.numExpands);
                 //Console.WriteLine("T0:{0}", t0);
                 //for (int i=0 ; i<DynamicRationalLazyOpenList.NUM_CAPS ; ++i)
-                    //Console.WriteLine("Ph for cap {0}:{1}={2}/{3}", ((double)(1 << i)) / 1000, this.capData[i][2], this.capData[i][0], this.capData[i][0] + this.capData[i][1]);
+                    //Console.WriteLine("Ph for cap {0}:{1}={2}/{3}", ((double)(1 << i)) / 1000, this.capData[i, 2], this.capData[i, 0], this.capData[i, 0] + this.capData[i, 1]);
                 //for (int i = 0; i < DynamicRationalLazyOpenList.NUM_CAPS; ++i)
                     //Console.WriteLine("Expected regret for cap {0}:{1}", ((double)(1 << i)) / 1000, expectedRegret[i]/1000.0);
 
+                if (node.g + node.h < lastF) // Must improve the heuristic estimate to be consistent
+                    millisCap = Double.MaxValue;
+
                 bool success = false;
-                if (millisCap > overhead || // Worth running the expensive heuristic
-                    node.g + node.h < lastF) // Must improve the heuristic estimate to be consistent
+                if (millisCap > overhead)  // Worth running the expensive heuristic
                 {
                     double expensiveCallStartTime = this.runner.ElapsedMilliseconds();
                     int expensiveEstimate = (int)this.expensive.h(node, targetH, -1, (int)(expensiveCallStartTime + millisCap), false);
@@ -169,31 +174,33 @@ namespace CPF_experiment
                         node.h = expensiveEstimate; // If this wasn't a success, the assignment here serves to force the next heuristic call
                                                     // to search deeper, since we're always consistent.
 
+                    this.capData[selectedCapExponent, DynamicRationalLazyOpenList.USES_IND]++;
+
                     success = node.CompareTo(next) == 1; // Node is not the smallest F anymore - re-insert it into the open list
                     if (success || node.GoalTest())
                     {
                         int lowestCapThatWouldHaveWorked = (int)Math.Ceiling(Math.Log(expensiveCallTotalTime * 1000, 2));
                         //Console.WriteLine("Lowest cap that would have worked:{0}", (1<<lowestCapThatWouldHaveWorked)/1000.0);
-                        for (int j = 0; j < lowestCapThatWouldHaveWorked; ++j)
+                        for (int j = 0; j < lowestCapThatWouldHaveWorked && j < DynamicRationalLazyOpenList.NUM_CAPS; ++j)
                         {
-                            this.capData[j][DynamicRationalLazyOpenList.FAILURE_IND] += 1;
-                            this.capData[j][DynamicRationalLazyOpenList.PH_IND] = (1 - DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR) * this.capData[j][DynamicRationalLazyOpenList.PH_IND] +
-                                                                                  DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR * this.capData[j][DynamicRationalLazyOpenList.SUCCESS_IND] / (this.capData[j][DynamicRationalLazyOpenList.SUCCESS_IND] + this.capData[j][DynamicRationalLazyOpenList.FAILURE_IND]);
+                            this.capData[j, DynamicRationalLazyOpenList.FAILURE_IND] += 1;
+                            this.capData[j, DynamicRationalLazyOpenList.PH_IND] = (1 - DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR) * this.capData[j, DynamicRationalLazyOpenList.PH_IND] +
+                                                                                  DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR * this.capData[j, DynamicRationalLazyOpenList.SUCCESS_IND] / (this.capData[j, DynamicRationalLazyOpenList.SUCCESS_IND] + this.capData[j, DynamicRationalLazyOpenList.FAILURE_IND]);
                         }
                         for (int j = lowestCapThatWouldHaveWorked; j < DynamicRationalLazyOpenList.NUM_CAPS; ++j)
                         {
-                            this.capData[j][DynamicRationalLazyOpenList.SUCCESS_IND] += 1;
-                            this.capData[j][DynamicRationalLazyOpenList.PH_IND] = (1 - DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR) * this.capData[j][DynamicRationalLazyOpenList.PH_IND] +
-                                                                                  DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR * this.capData[j][DynamicRationalLazyOpenList.SUCCESS_IND] / (this.capData[j][DynamicRationalLazyOpenList.SUCCESS_IND] + this.capData[j][DynamicRationalLazyOpenList.FAILURE_IND]);
+                            this.capData[j, DynamicRationalLazyOpenList.SUCCESS_IND] += 1;
+                            this.capData[j, DynamicRationalLazyOpenList.PH_IND] = (1 - DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR) * this.capData[j, DynamicRationalLazyOpenList.PH_IND] +
+                                                                                  DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR * this.capData[j, DynamicRationalLazyOpenList.SUCCESS_IND] / (this.capData[j, DynamicRationalLazyOpenList.SUCCESS_IND] + this.capData[j, DynamicRationalLazyOpenList.FAILURE_IND]);
                         }
                     }
                     else
                     {
                         for (int j = 0 ; j <= selectedCapExponent ; ++j)
                         {
-                            this.capData[j][DynamicRationalLazyOpenList.FAILURE_IND] += 1;
-                            this.capData[j][DynamicRationalLazyOpenList.PH_IND] = (1 - DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR) * this.capData[j][DynamicRationalLazyOpenList.PH_IND] +
-                                                                                  DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR * this.capData[j][DynamicRationalLazyOpenList.SUCCESS_IND] / (this.capData[j][DynamicRationalLazyOpenList.SUCCESS_IND] + this.capData[j][DynamicRationalLazyOpenList.FAILURE_IND]); // This is only necessary for the first runs so the Ph won't jitter too much.
+                            this.capData[j, DynamicRationalLazyOpenList.FAILURE_IND] += 1;
+                            this.capData[j, DynamicRationalLazyOpenList.PH_IND] = (1 - DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR) * this.capData[j, DynamicRationalLazyOpenList.PH_IND] +
+                                                                                  DynamicRationalLazyOpenList.MOVING_AVERAGE_FACTOR * this.capData[j, DynamicRationalLazyOpenList.SUCCESS_IND] / (this.capData[j, DynamicRationalLazyOpenList.SUCCESS_IND] + this.capData[j, DynamicRationalLazyOpenList.FAILURE_IND]); // This is only necessary for the first runs so the Ph won't jitter too much.
                         }
                         // No info on whether a larger cap would have worked.
                     }
@@ -231,6 +238,14 @@ namespace CPF_experiment
             output.Write(this.ToString() + " Average Texpand (ms)");
             output.Write(Run.RESULTS_DELIMITER);
 
+            for (int i = 0; i < NUM_CAPS; ++i)
+            {
+                output.Write(this.ToString() + " Ph(2**" + i + " microseconds)");
+                output.Write(Run.RESULTS_DELIMITER);
+                output.Write(this.ToString() + " 2**" + i + " microseconds cap uses)");
+                output.Write(Run.RESULTS_DELIMITER);
+            }
+
             this.expensive.OutputStatisticsHeader(output);
         }
 
@@ -250,6 +265,18 @@ namespace CPF_experiment
             output.Write(this.skips + Run.RESULTS_DELIMITER);
             output.Write(Texpand + Run.RESULTS_DELIMITER);
 
+            // Phelfuls:
+            for (int i = 0; i < NUM_CAPS; ++i)
+            {
+                Console.WriteLine(this.ToString() + " Ph(2**{0} microseconds): {1}", i, this.capData[i, PH_IND]);
+                Console.WriteLine(this.ToString() + " 2**{0} microseconds cap uses: {1}", i, this.capData[i, USES_IND]);
+
+                output.Write(this.capData[i, PH_IND]);
+                output.Write(Run.RESULTS_DELIMITER);
+                output.Write(this.capData[i, USES_IND]);
+                output.Write(Run.RESULTS_DELIMITER);
+            }
+
             this.expensive.OutputStatistics(output);
         }
 
@@ -257,7 +284,7 @@ namespace CPF_experiment
         {
             get
             {
-                return 2 + base.NumStatsColumns + this.expensive.NumStatsColumns;
+                return 2 + 2 * NUM_CAPS + base.NumStatsColumns + this.expensive.NumStatsColumns;
             }
         }
 
@@ -274,7 +301,7 @@ namespace CPF_experiment
         {
             base.ClearAccumulatedStatistics();
 
-            this.accSkips = 0;
+            this.ClearPrivateAccumulatedStatistics();
 
             this.expensive.ClearAccumulatedStatistics();
         }
@@ -287,6 +314,10 @@ namespace CPF_experiment
             this.accNumExpands += this.numExpands;
             this.accSumExpandTimes += this.sumExpandTimes;
 
+            for (int i = 0; i < this.capData.GetLength(0); ++i)
+                for (int j = 0; j < this.capData.GetLength(1); ++j)
+                    this.accCapData[i, j] += this.capData[i, j];
+
             this.expensive.AccumulateStatistics();
         }
 
@@ -295,7 +326,7 @@ namespace CPF_experiment
             base.OutputAccumulatedStatistics(output);
 
             double accTexpand;
-            if (this.numExpands != 0)
+            if (this.accNumExpands != 0)
                 accTexpand = this.accSumExpandTimes / this.accNumExpands;
             else
                 accTexpand = -1;
@@ -305,6 +336,23 @@ namespace CPF_experiment
 
             output.Write(this.accSkips + Run.RESULTS_DELIMITER);
             output.Write(accTexpand + Run.RESULTS_DELIMITER);
+
+            // Phelfuls:
+            for (int i = 0; i < NUM_CAPS; ++i)
+            {
+                double denom = this.accCapData[i, SUCCESS_IND] + this.accCapData[i, FAILURE_IND];
+                if (denom != 0)
+                    this.accCapData[i, PH_IND] = this.accCapData[i, SUCCESS_IND] / denom;
+                else
+                    this.accCapData[i, PH_IND] = -1;
+                Console.WriteLine(this.ToString() + " Accumulated Ph(2**{0} microseconds): {1}", i, this.accCapData[i, PH_IND]);
+                Console.WriteLine(this.ToString() + " Accumulated 2**{0} microseconds cap uses: {1}", i, this.accCapData[i, USES_IND]);
+
+                output.Write(this.accCapData[i, PH_IND]);
+                output.Write(Run.RESULTS_DELIMITER);
+                output.Write(this.accCapData[i, USES_IND]);
+                output.Write(Run.RESULTS_DELIMITER);
+            }
 
             this.expensive.OutputAccumulatedStatistics(output);
         }
