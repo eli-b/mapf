726c7a78f0132a24458d35674070e225739ba982
Eli Boyarski <Eli Boyarski@localhost>
2016-08-25 22:44:31 +0300

    Print the exception on bad rows when running runtimes_and_successes.py

--- bin/Debug/runtimes_and_successes.py
+++ bin/Debug/runtimes_and_successes.py
@@ -41,7 +41,7 @@ for i, reader in enumerate(readers):
             solvers_that_succeeded = [col_name[:-len(" Success")] for col_name, col_val in row.iteritems() \
                                       if col_name in success_fieldnames if int(col_val) == 1]
         except (TypeError, ValueError) as e:
-            print "Problem in row: num agents=", row["Num Of Agents"], " instance id=", row["Instance Id"]
+            print "Problem in row: num agents=", row["Num Of Agents"], " instance id=", row["Instance Id"], e
             #raise
             continue
         solver_run_count_per_num_of_agents[num_of_agents].update(solvers_run) # Add the counts of the items in solvers
@@ -153,7 +153,7 @@ for i, reader in enumerate(readers):
                                              if solver_name in relevant_solvers_in_category}
             
         except (TypeError, ValueError) as e:
-            print "Problem in row: num agents=", row["Num Of Agents"], " instance id=", row["Instance Id"]
+            print "Problem in row: num agents=", row["Num Of Agents"], " instance id=", row["Instance Id"], e
             #raise
             continue
         
